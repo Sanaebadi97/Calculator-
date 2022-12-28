230ba4d5f071585f2a05cdffc978d384e461ffbe
sanaebadi <sana@journeyhero.net>
2022-12-28 20:25:28 +0200

    completed the java code

--- app/src/main/java/info/sanaebadi/calculator/MainActivity.java
+++ app/src/main/java/info/sanaebadi/calculator/MainActivity.java
@@ -1,15 +1,61 @@
 package info.sanaebadi.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import info.sanaebadi.calculator.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ActivityMainBinding binding;
+    private Double firstNumber, secondNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonPlus.setOnClickListener(view -> {
+            firstNumber = Double.parseDouble(binding.edittextFirstNumber.getText().toString());
+            secondNumber = Double.parseDouble(binding.edittextSecondNumber.getText().toString());
+
+            binding.textviewResult.setText(firstNumber + secondNumber + "");
+
+
+        });
+
+
+        binding.buttonDivsion.setOnClickListener(view -> {
+            firstNumber = Double.parseDouble(binding.edittextFirstNumber.getText().toString());
+            secondNumber = Double.parseDouble(binding.edittextSecondNumber.getText().toString());
+
+            binding.textviewResult.setText(firstNumber / secondNumber + "");
+
+
+        });
+
+        binding.buttonMinus.setOnClickListener(view -> {
+            firstNumber = Double.parseDouble(binding.edittextFirstNumber.getText().toString());
+            secondNumber = Double.parseDouble(binding.edittextSecondNumber.getText().toString());
+
+            binding.textviewResult.setText(firstNumber - secondNumber + "");
+
+
+        });
+
+
+        binding.buttonMultiply.setOnClickListener(view -> {
+            firstNumber = Double.parseDouble(binding.edittextFirstNumber.getText().toString());
+            secondNumber = Double.parseDouble(binding.edittextSecondNumber.getText().toString());
+
+            binding.textviewResult.setText(firstNumber * secondNumber + "");
+
+
+        });
+
 
     }
 }
